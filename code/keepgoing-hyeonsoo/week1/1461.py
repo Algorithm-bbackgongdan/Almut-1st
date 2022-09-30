76fdd8284bacaa367d38161f29a255e0b57fc3d1
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-09-30 22:53:27 +0900

    1461 수정

--- code/keepgoing-hyeonsoo/week1/1461.py
+++ code/keepgoing-hyeonsoo/week1/1461.py
@@ -4,19 +4,35 @@ read = sys.stdin.readline
 
 n, m = map(int, read().split())
 books = list(map(int, read().split()))
-minn = 10001
-now = 0
 res = 0
+plus = []
+minus = []
+maxx = 0
 
+for book in books:
+    if book > 0:
+        plus.append(book)
+    else:
+        minus.append(book)
 
-while len(books) > 0:
-    for i in range(len(books)):
-        if abs(books[i] - now) < minn:
-            minn = abs(books[i] - now)
-            minn_idx = i
-    res += minn
-    now = books[i]
-    del books[minn_idx]
+    if abs(book) > abs(maxx):
+        maxx = book
 
+plus.sort(reverse=True)
+minus.sort()
+
+distances = []
+
+for i in range(0, len(plus), m):
+    if plus[i] != maxx:
+        distances.append(plus[i])
+
+for i in range(0, len(minus), m):
+    if minus[i] != maxx:
+        distances.append(minus[i])
+
+res = abs(maxx)
+for i in distances:
+    res += abs(i * 2)
 
 print(res)
