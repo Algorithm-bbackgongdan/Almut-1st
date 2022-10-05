67dfaf228a75620c78c54cd9397c2632c4139538
jonghyeon jo <hyeon058@naver.com>
2022-10-06 02:20:36 +0900

    2504

--- code/jonghyeonjo99/week2/2504.py
+++ code/jonghyeonjo99/week2/2504.py
@@ -1,37 +1,36 @@
-a = input()
-stack = []
-res = 0
-temp = 1
-count1 = count2 = count3 = count4 = 0
+stack = [] 
+temp = 1 
+result = 0 
+a = list(input()) 
+
 
 for i in range(len(a)):
-  if a[i] == "(":
-    count1 += 1
-  elif a[i] == ")":
-    count2 += 1
-  elif a[i] == "[":
-    count3 += 1
-  else:
-    count4 +=1
-if count1 != count2 or count3 != count4:
-  print(0) #(,[의 숫자와 ],)의 숫자가 다르면 짝이 맞지않는다.
+  if a[i]=='(':
+    temp *= 2
+    stack.append(a[i])
+    
+  elif a[i]=='[':
+    temp *= 3
+    stack.append(a[i])
+    
+  elif a[i]==')':
+    if not stack or stack[-1]!='(':
+      result = 0
+      break
+    if a[i-1]=='(': result += temp
+    temp //= 2
+    stack.pop()
+    
+  elif a[i]==']':
+    if not stack or stack[-1]!='[':
+      result = 0
+      break
+    if a[i-1]=='[': result += temp
+    temp //= 3
+    stack.pop()
 
+if stack:
+  print(0)
 else:
-  for i in range(len(a)):
-    if a[i] == "(":
-      stack.append(a[i])
-      temp *= 2
-    elif a[i] == "[":
-      stack.append(a[i])
-      temp *= 3
-    elif a[i] == ")":
-      if a[i-1] == "(":
-        res += temp
-      stack.pop()
-      temp //= 2
-    else:
-      if a[i-1] == "[":
-        res += temp
-      stack.pop()
-      temp //= 3
-  print(res)
+  print(result)
+  # 괄호 쌍이 맞지않는 케이스를 stack으로 구현
