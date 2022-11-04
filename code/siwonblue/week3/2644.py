4f715e37f7b22437c3a6bdfe7c14884a791e0137
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-11-04 22:22:17 +0900

    BOJ 2644_s2

---
+++ code/siwonblue/week3/2644.py
@@ -0,0 +1,34 @@
+# 촌수계산
+# 22.11.2
+# bdfs
+from collections import deque
+n = int(input())
+start, end = map(int,input().split())
+m = int(input())
+graph = [[] for _ in range(n)]
+visited = [False] * n
+for _ in range(m):
+  a, b = map(int,input().split())
+  graph[a-1].append(b)
+  graph[b-1].append(a)
+for node in graph:
+  node.sort()
+# print(graph)
+cnt = 0
+flag = False
+def dfs(node):
+  global flag
+  global cnt
+  visited[node-1] = True
+  cnt +=1
+  if node == end:
+    flag = True
+    print(cnt-1)
+    return
+  for v in graph[node-1]:
+    if visited[v-1] == False:
+      dfs(v)
+dfs(start)
+
+if not flag:
+  print('-1')
