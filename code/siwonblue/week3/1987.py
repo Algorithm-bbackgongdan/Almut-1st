b2b54aac211f4e36ed2f0e1c244aaff279333afb
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2022-11-06 10:48:47 +0900

    BOJ 1987 trying

--- code/siwonblue/week3/1987.py
+++ code/siwonblue/week3/1987.py
@@ -5,10 +5,27 @@ import sys
 read=sys.stdin.readline
 r,c=map(int,read().split())
 
-alpabet = [ list(input()) for _ in range(r)]
-temp = []
-for v in alpabet:
-  temp+=v
-temp = list(set(temp))
-print(temp)
+visited = [[False]*c for _ in range(r)]
+visited[0][0]=1
+alphabet = [ list(input()) for _ in range(r)]
 
+dx=[-1,0,1,0] 
+dy=[0,1,0,-1]
+test = []
+cnt = 0
+ans = 0
+def dfs(x,y,cnt):
+  global ans
+  ans = max(cnt,ans)
+  now = alphabet[x][y]
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx <0 or ny <0 or nx>=r or ny >=c:
+      continue
+    if not visited[nx][ny] and alphabet[nx][ny] not in test:
+      test.append(alphabet[nx][ny])
+      dfs(nx,ny,cnt+1)
+
+dfs(0,0,1)
+print(f'ans:{ans}')
