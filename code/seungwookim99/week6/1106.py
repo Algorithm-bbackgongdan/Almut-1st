af6b73b326dd063c71abbce8230b1bdb0d5d8f6f
Seungwoo Kim <sonicdx886@gmail.com>
2022-11-26 01:12:38 +0900

    week6

---
+++ code/seungwookim99/week6/1106.py
@@ -0,0 +1,18 @@
+import sys
+
+C, N = map(int, sys.stdin.readline().rstrip().split())
+cities = [
+    tuple(map(int,
+              sys.stdin.readline().rstrip().split())) for _ in range(N)
+]
+cities = sorted(cities, key=lambda x: (x[0], x[1]))
+INF = int(1e9)
+DP = [INF] * (C + 101)
+DP[0] = 0
+
+for i in range(N):
+    cost, benefit = cities[i]
+    for j in range(benefit, C + 101):
+        DP[j] = min(DP[j - benefit] + cost, DP[j])
+
+print(min(DP[C:]))
